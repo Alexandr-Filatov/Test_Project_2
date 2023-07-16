c6391d3361ce2a0b29ede6b15f9c716f67f83755
Alexandr-Filatov <alexandr.filatov006@gmail.com>
2023-07-16 22:51:41 +0300

    Первый рабочий вариант. Надо разобраться с задержкой

--- locators/form_page_locators.py
+++ locators/form_page_locators.py
@@ -1,13 +1,18 @@
+from selenium.webdriver.common.by import By
+from random import randint
+
 class FormPageLocators:
-    FIRST_NAME =
-    LAST_NAME =
-    EMAIL =
-    GENDER =
-    MOBILE =
-    SUBJECT =
-    FILE_INPUT =
-    CURRENT_ADDRESS =
-    SUBMIT =
+    FIRST_NAME = (By.ID, "firstName")
+    LAST_NAME = (By.ID, "lastName")
+    EMAIL = (By.ID, "userEmail")
+    GENDER = (By.CSS_SELECTOR, f"label[for='gender-radio-{randint(1,3)}']")
+    MOBILE = (By.ID, "userNumber")
+    DATE_OF_BIRTH = (By.ID, "dateOfBirthInput")
+    SUBJECT = (By.ID, "subjectsInput")
+    HOBBIES = (By.CSS_SELECTOR, f"label[for='hobbies-checkbox-{randint(1,3)}']")
+    FILE_INPUT = (By.ID, "uploadPicture")
+    CURRENT_ADDRESS = (By.ID, "currentAddress")
+    SUBMIT = (By.ID, "submit")
 
 
-    RESULT_TABLE = 
+    RESULT_TABLE = (By.XPATH, "//div[@class='table-responsive']//td[2]")
--- pages/form_page.py
+++ pages/form_page.py
@@ -1,7 +1,29 @@
+import time
+
+from selenium.webdriver import Keys
+
 from pages.base_page import BasePage
+from locators.form_page_locators import FormPageLocators as Locators
 
 
 class FormPage(BasePage):
 
     def fill_fields_and_submit(self):
-        pass
+        first_name = "Alex"
+        last_name = "Filatov"
+        email = "alexfilatov@gmail.com"
+        mobile = "1234567890"
+        self.element_is_visible(Locators.FIRST_NAME).send_keys(first_name)
+        self.element_is_visible(Locators.LAST_NAME).send_keys(last_name)
+        self.element_is_visible(Locators.EMAIL).send_keys(email)
+        self.element_is_visible(Locators.GENDER).click()
+        self.element_is_visible(Locators.MOBILE).send_keys(mobile)
+        subject = self.element_is_visible(Locators.SUBJECT)
+        subject.send_keys("English")
+        subject.send_keys(Keys.RETURN)
+        self.element_is_visible(Locators.HOBBIES).click()
+        self.element_is_visible(Locators.FILE_INPUT).send_keys(r"C:\Users\alexa\PycharmProjects\Test_Project_2\sending_file")
+        self.element_is_visible(Locators.CURRENT_ADDRESS).send_keys("Russia, Moscow")
+        self.element_is_visible(Locators.SUBMIT).click()
+        time.sleep(10)
+
